93b2612c7dce75c4291805f1c3dc95e0a0c20ec1
zcz12345 <56070057@kmitl.ac.th>
2018-05-14 16:00:24 +0700

    Revert "fix bug"
    This reverts commit 5b21219970d9790e6d51bd1c2d9f1592e7a9b8de.

--- models/db.js
+++ models/db.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 let users = require('./users')
 let areas = require('./areas')
 let tablet = require('./tablet')
+let progress = require('./progress')
 let sn1 = require('./SN1/SN1')
 let sn2_2 = require('./SN2_2/SN2_2')
 
